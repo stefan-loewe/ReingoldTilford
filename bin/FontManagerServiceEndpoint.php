17deeb52f652c5326538c05da3829e706349cd28
stefan-loewe <stefan@loewe.ws>
2013-03-17 23:09:56 +0100

    autoloader now is always looked for in vendor subfolder of application folder - if the look-up added actually works
    also updated to current code formatting style

--- bin/FontManagerServiceEndpoint.php
+++ bin/FontManagerServiceEndpoint.php
@@ -9,26 +9,35 @@ use \ws\loewe\Utils\Graphics2D\Shapes\Styles\TextStyle;
 use \ws\loewe\Utils\Font\FontManager;
 use \ws\loewe\Utils\Font\FontMetric;
 
-require __DIR__.'/../vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'].'/'.getApplicationFolder().'/vendor/autoload.php';
 
 $result = '';
 
-if(php_sapi_name() !== 'cli')
-{
-    if(array_key_exists('fontDefinitions', $_GET))
-    {
-        $textStyle = new TextStyle($_GET['fontFamily'], $_GET['fontSize']);
-
-        $fontManager = new FontManager(null);
+if(php_sapi_name() !== 'cli') {
+    if(array_key_exists('fontDefinitions', $_GET)) {
+        $textStyle    = new TextStyle($_GET['fontFamily'], $_GET['fontSize']);
+        $fontManager  = new FontManager(null);
 
         $fontManager->addFontMetric(new FontMetric($textStyle, explode(',', $_GET['fontDefinitions'])));
 
         $result = $textStyle.' added successfully!';
     }
-    else
+    else {
         $result = 'no font defintions given!';
+    }
 }
-else
+else {
     $result = 'please run this script from a web browser!';
+}
 
 echo '"'.$result.'"';
+
+
+function getApplicationFolder() {
+  $folder = null;
+  if(isset($_SERVER['REQUEST_URI'])) {
+    $folder = substr($_SERVER['REQUEST_URI'], 1, strpos($_SERVER['REQUEST_URI'], '/', 1) - 1);
+  }
+
+  return $folder;
+}
